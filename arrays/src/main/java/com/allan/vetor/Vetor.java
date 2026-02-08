45ae01a97cc92723330045586d577c9a426f4539
AllanGaBRs <allanmediogames@gmail.com>
2026-02-08 19:04:34 -0300

    arrays

---
+++ arrays/src/main/java/com/allan/vetor/Vetor.java
@@ -0,0 +1,9 @@
+package com.allan.vetor;
+
+public class Vetor {
+    private String[] elements;
+
+    public Vetor(int capacity){
+        this.elements = new String[capacity];
+    }
+}
---
+++ arrays/src/main/java/com/allan/vetor/test/Main01.java
@@ -0,0 +1,13 @@
+package com.allan.vetor.test;
+
+import com.allan.vetor.Vetor;
+
+public class Main01 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+    }
+}
