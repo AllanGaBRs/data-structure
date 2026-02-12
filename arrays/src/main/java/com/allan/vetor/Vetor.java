5ae07ae88a0294be6d7244c1e30dc58ddc540fa0
AllanGaBRs <allanmediogames@gmail.com>
2026-02-11 21:08:21 -0300

    feat: add element to the end the vector

--- arrays/src/main/java/com/allan/vetor/Vetor.java
+++ arrays/src/main/java/com/allan/vetor/Vetor.java
@@ -2,8 +2,37 @@ package com.allan.vetor;
 
 public class Vetor {
     private String[] elements;
+    private int size;
 
     public Vetor(int capacity){
         this.elements = new String[capacity];
+        this.size = 0;
+    }
+
+    /*public void adds(String element){
+        for(int i = 0; i < this.elements.length; i++){
+            if(this.elements[i] == null){
+                this.elements[i] = element;
+                break;
+            }
+        }
+    }*/
+
+    /*public void adds(String element) throws Exception{
+        if(this.size < this.elements.length){
+            this.elements[this.size] = element;
+            this.size++;
+        } else {
+            throw new Exception("The array is already full, it's not possible to add more items.");
+        }
+    }*/
+
+    public boolean adds(String element) {
+        if(this.size < this.elements.length){
+            this.elements[this.size] = element;
+            this.size++;
+            return true;
+        }
+        return false;
     }
 }
---
+++ arrays/src/main/java/com/allan/vetor/test/Main02.java
@@ -0,0 +1,13 @@
+package com.allan.vetor.test;
+
+import com.allan.vetor.Vetor;
+
+public class Main02 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(2);
+        vetor.adds("Elemento 1");
+        vetor.adds("Elemento 2");
+        vetor.adds("Elemento 3");
+    }
+}
